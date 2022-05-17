7e643dfe960dec85f30118b4663b550ce84f7d09
Najmrodzka <101128573+Najmrodzka@users.noreply.github.com>
2022-05-17 10:07:14 +0200

    Update ProjektIV.cpp

--- ProjektIV/ProjektIV.cpp
+++ ProjektIV/ProjektIV.cpp
@@ -21,9 +21,7 @@ void createFile(const string name, const int count, const char value) {
 
     for (i = 0; i < count; i++) {
         MyFile << value;
-
     }
-
 };
 
 void createFileRandom(const string name, const int count, const char value1, const char value2) {
@@ -32,14 +30,24 @@ void createFileRandom(const string name, const int count, const char value1, con
 
     for (i = 0; i < 90; i++) {
         MyFile << value1;
-
     }
     for (i = 90; i < count; i++) {
         MyFile << value2;
-
     }
 };
 
+uint8_t calculateHamingDistance(uint8_t n1, uint8_t n2)
+{
+    uint8_t x = n1 ^ n2;
+    uint8_t setBits = 0;
+
+    while (x > 0)
+    {
+        setBits += x & 1;
+        x >>= 1;
+    }
+    return setBits;
+};
 
 int main()
 {
