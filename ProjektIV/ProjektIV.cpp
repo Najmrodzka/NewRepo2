dd3f91413746bd80048fe506264074be14714a4b
Najmrodzka <101128573+Najmrodzka@users.noreply.github.com>
2022-05-20 12:30:04 +0200

    Update ProjektIV.cpp

--- ProjektIV/ProjektIV.cpp
+++ ProjektIV/ProjektIV.cpp
@@ -117,13 +117,6 @@ void CompareFunction(const char* FirstFile, const char* SecondFile) {
 
 int main(int argc, char* argv[])
 {
-    /* createFile("100bytes_1.bin", 100, 0x55);
-    createFile("100bytes_2.bin", 100, 0x55);
-    createFileRandom("100bytes_3_changed_by_10_bytes.bin", 100, 0x55, 0x45);
-    createFile("400MB_1.bin", 419430400, 0x55);
-    createFile("400MB_2.bin", 419430400, 0x50);
-    cout << "wykonano create file";
-    */
     std::cout << "BER Checker!" << std::endl;
     std::cout << "argc = : " << argc << std::endl;
 
